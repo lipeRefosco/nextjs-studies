e6d5d0862afbbbcb86eacfa538dea34e66325369
lipe <luisfelipe.refosco@hotmail.com>
2023-05-21 12:24:48 -0300

    creating a static route

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="w-fit flex flex-col justify-center text-center h-screen m-auto border border-green-300">
+        {children}
+        </main>
+      </body>
     </html>
   )
 }
---
+++ my-app/src/app/mypage/page.tsx
@@ -0,0 +1,5 @@
+import Link from 'next/link'
+
+export default function MyPage() {
+	return <Link href="/">Home</Link>
+}
