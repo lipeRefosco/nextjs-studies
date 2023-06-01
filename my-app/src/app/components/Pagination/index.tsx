0395c15b18d4c6ae908fcfc8d555b685d071d3ff
lipe <luisfelipe.refosco@hotmail.com>
2023-05-31 21:07:28 -0300

    fix: add disabled link on the first page of pagination list

--- my-app/src/app/components/Pagination/index.tsx
+++ my-app/src/app/components/Pagination/index.tsx
@@ -13,16 +13,18 @@ export default function Pagination(
     }
 ) {
     const totalPages: Array<number> = [...new Array(totalUsers / usersPerPage).keys()];
-
+    
     return (
         <div className="m-2 flex justify-end">
-            {totalPages.map((val) => {
+            {totalPages.map((page:number) => {
+                const onFirstPage = searchParams.page === undefined && page === 0
+                const onPage = parseInt(searchParams.page) === page;
 
-                const onPage = parseInt(searchParams.page) === val
+                console.log(onPage)
 
-                return onPage
-                        ? <span key={val} className="pagination-number pagination-number--disabled">{val + 1}</span>
-                        : <Link key={val} href={'?page=' + val} className="pagination-number">{val + 1}</Link>
+                return onPage || onFirstPage
+                        ? <span key={page} className="pagination-number pagination-number--disabled">{page + 1}</span>
+                        : <Link key={page} href={'?page=' + page} className="pagination-number">{page + 1}</Link>
             })}
         </div>
     )
